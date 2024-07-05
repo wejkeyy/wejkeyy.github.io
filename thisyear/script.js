ab2321724bd47c9fbd96c494c592f8293f732a33
Wejkey <realwejkey@gmail.com>
2024-07-05 12:59:20 +0200

    Update script.js

--- thisyear/script.js
+++ thisyear/script.js
@@ -1,11 +1,19 @@
-var now = new Date();
-var startOfYear = new Date(now.getFullYear(), 0, 1);
-var difference = now - startOfYear;
-var millisecondsInYear = 1000 * 60 * 60 * 24 * 365.25; // Considering leap years
-var percentage = (difference / millisecondsInYear) * 100;
-
-percentage = percentage.toFixed(2);
-document.getElementById('percentage').innerHTML = `<p>${percentage}% of the year has passed.</p>`;
-
-var progressBar = document.getElementById('progressBar');
-progressBar.style.width = percentage + '%';
+function updatePercentage() {
+    var now = new Date();
+    var startOfYear = new Date(now.getFullYear(), 0, 1);
+    var difference = now - startOfYear;
+    var millisecondsInYear = 1000 * 60 * 60 * 24 * 365.25;
+    var percentage = (difference / millisecondsInYear) * 100;
+    percentage = percentage.toFixed(2);
+  
+    var oneDay = 1000 * 60 * 60 * 24;
+    var dayOfYear = Math.floor(difference / oneDay) + 1;
+  
+    document.getElementById('percentage').innerHTML = `<p>${percentage}% of the year has passed. (${dayOfYear}/365)</p>`;
+    var progressBar = document.getElementById('progressBar');
+    progressBar.style.width = percentage + '%';
+  }
+  
+  updatePercentage();
+  
+  setInterval(updatePercentage, 1000);  
