091259a31a2ab8f97db397d1cd4343936ad4ffe5
Wejkey <174510694+wejkeyy@users.noreply.github.com>
2024-07-22 15:43:10 +0200

    Update script.js

--- thisschool/script.js
+++ thisschool/script.js
@@ -2,14 +2,18 @@ function updateHolidayPercentage() {
     var now = new Date();
     var startOfHolidays = new Date(2024, 5, 29, 0, 0, 0); 
     var endOfHolidays = new Date(2024, 8, 2, 0, 0, 0); 
-    var totalHolidayTime = endOfHolidays - startOfHolidays;
-    var passedHolidayTime = now - startOfHolidays;
+    
+    var totalHolidayTime = endOfHolidays - startOfHolidays; 
+    var passedHolidayTime = now - startOfHolidays; 
     
     var percentage = (passedHolidayTime / totalHolidayTime) * 100;
     if (percentage > 100) {
         percentage = 100; 
+    } else if (percentage < 0) {
+        percentage = 0;
     }
-    percentage = percentage.toFixed(2);
+    
+    percentage = percentage.toFixed(2); 
     
     document.getElementById('percentage').innerHTML = `<p>${percentage}% of the holidays have passed.</p>`;
     var progressBar = document.getElementById('progressBar');
